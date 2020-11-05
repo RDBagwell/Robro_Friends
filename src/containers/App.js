6b0e313a71a9dd0b95d5cb2f84f053d152f67ecd
Robert Bagwell <erbagwell@yahoo.com>
2020-11-04 23:35:46 -0700

    Redux Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-// import {connect} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import CardList from '../components/CardList/CardList';
 import SearchBox from '../components/SearchBox/SearchBox';
 import Scroll from '../components/Scroll/Scroll';
 import ErrorBoundry from '../components/EorrorBoundry/ErrorBoundry'
 import './App.css';
 
-// import { setSearchField } from '../actions';
+import { setSearchField } from '../actions';
 
 // const mapStateToProps = state =>{
 //     return {
@@ -20,30 +20,28 @@ import './App.css';
 //     }
 // }
 
-function APP({state}) {
+function APP({store}) {
 
     const [robots, setRobots] = useState([]);
     const [searchfield, setSearchfield] = useState('');
+    const text = useSelector(state => state.searchRobots.searchfield);
+    const dispatch = useDispatch();
 
+    const onSearchChange = (event)=>{
+        setSearchfield(event.target.value)
+    }
 
     useEffect( ()=>{
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        // .then(res => res.json())
-        // .then(users => {setRobots(users)});
         async function fetchData(){
             const res = await fetch('https://jsonplaceholder.typicode.com/users');
             const users = await res.json();
             setRobots(users)
         }
         fetchData();
-    }, [])
-
-    const onSearchChange = (event)=>{
-        setSearchfield(event.target.value)
-    }
+    }, []);
 
     const filterRobots = robots.filter(robot => {
-        return robot.name.toLocaleLowerCase().includes(searchfield.toLowerCase());
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
     if(robots.length === 0){
